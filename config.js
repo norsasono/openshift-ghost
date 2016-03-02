1efc0a1fbd65c183d77155ba6d95d2c779cf68da
scream3r <scream3r.org@gmail.com>
2016-03-02 09:36:52 +0300

    OpenShift prepared config (only production + sqlite + disabled 'useUpdateCheck')

---
+++ config.js
@@ -0,0 +1,36 @@
+// This configuration file contains only production + sqlite settings with disabled 'useUpdateCheck'.
+// For additional settings please check config.example.js
+
+var path = require('path');
+var config;
+
+var productionSettings = {
+    url: process.env.OPENSHIFT_APP_DNS || 'my-ghost-blog.com',
+    server: {
+        host: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+        port: process.env.OPENSHIFT_NODEJS_PORT || '2368'
+    }
+};
+
+config = {
+    production: {
+        url: 'http://' + productionSettings.url,
+        mail: {},
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost.db')
+            },
+            debug: false
+        },
+        server: {
+            host: productionSettings.server.host,
+            port: productionSettings.server.port
+        },
+        privacy: {
+            useUpdateCheck: false
+        }
+    }
+};
+
+module.exports = config;
